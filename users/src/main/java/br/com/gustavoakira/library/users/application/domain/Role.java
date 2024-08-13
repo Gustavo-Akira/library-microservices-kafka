cd0f73995798815efbc818276b283836b267284d
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2024-08-12 21:56:49 -0300

    fix: role domain test

--- users/src/main/java/br/com/gustavoakira/library/users/application/domain/Role.java
+++ users/src/main/java/br/com/gustavoakira/library/users/application/domain/Role.java
@@ -1,4 +1,16 @@
 package br.com.gustavoakira.library.users.application.domain;
 
 public class Role {
+    private Long id;
+    private String name;
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public  Role(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
 }
--- users/src/test/java/br/com/gustavoakira/library/users/application/domain/RoleTest.java
+++ users/src/test/java/br/com/gustavoakira/library/users/application/domain/RoleTest.java
@@ -9,20 +9,20 @@ class RoleTest {
 
     @Test
     void shouldThrowValidationExceptionWhenNameIsNull(){
-         assertThrows(Exception.class, new Role(null));
-        assertThrows(Exception.class, new Role(1L,null));
+         assertThrows(Exception.class,()-> new Role(null));
+        assertThrows(Exception.class, ()->new Role(1L,null));
     }
 
     @Test
     void shouldThrowValidationExceptionWhenNameIsEmpty(){
-        assertThrows(Exception.class, new Role(""));
-        assertThrows(Exception.class, new Role(1L,""));
+        assertThrows(Exception.class, ()->new Role(""));
+        assertThrows(Exception.class, ()->new Role(1L,""));
     }
 
     @Test
     void shouldCreateRoleWhenNameIsValid(){
-        assertThrows(Exception.class, new Role("admin"));
-        assertThrows(Exception.class, new Role(1L,"admin"));
+        assertDoesNotThrow(()->new Role("admin"));
+        assertDoesNotThrow(()->new Role(1L,"admin"));
     }
 
 }
